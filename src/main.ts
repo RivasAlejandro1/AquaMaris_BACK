8bda6f4ccb6d3ec76f14281f57b4999e18c74b10
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-06-30 19:42:08 -0600

    Modiqued createRoom.dto

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+   }),  
+  );
   await app.listen(3001);
 }
 bootstrap();
