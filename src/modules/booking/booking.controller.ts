7b1f6458dc9f0fe9256d31f358db3c9f616e8cc3
Joshua <joshuarodriguez190622@gmail.com>
2024-07-11 15:13:54 -0600

    ADD cancel reservation

--- src/modules/booking/booking.controller.ts
+++ src/modules/booking/booking.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Get, Param, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { MakeBookingDto } from 'src/dtos/MakeBooking.dto';
-import {  dataBookingDatesInterceptor } from 'src/interceptors/dataBookingDates.interceptor';
+import { dataBookingDatesInterceptor } from 'src/interceptors/dataBookingDates.interceptor';
 import { areIntervalsOverlapping, interval, parseISO } from 'date-fns';
 
 @Controller('booking')
@@ -13,32 +20,40 @@ export class BookingController {
     await this.bookingService.bookingSeeder();
   }
 
-  @Get("forMonths")
-  async findBookingsByMonths(@Body() infoRango){
-    const { rango } = infoRango
-    return await this.bookingService.findBookingsByMonths(rango)
+  @Get('forMonths')
+  async findBookingsByMonths(@Body() infoRango) {
+    const { rango } = infoRango;
+    return await this.bookingService.findBookingsByMonths(rango);
   }
 
-  @Get("forMonths/typeRoom")
-  async findBookingsByMonthsWithTypeRoom(@Body() infoRango){
-    const { rango } = infoRango
-    return await this.bookingService.findBookingsByMonthsWithTypeRoom(rango)
+  @Get('forMonths/typeRoom')
+  async findBookingsByMonthsWithTypeRoom(@Body() infoRango) {
+    const { rango } = infoRango;
+    return await this.bookingService.findBookingsByMonthsWithTypeRoom(rango);
   }
 
-  @Get("forMonths/typeRoom/porcent")
-  async findBookingsByMonthsWithTypeRoomPorcent(@Body() infoRango){
-    const { rango } = infoRango
-    return await this.bookingService.findBookingsByMonthsWithTypeRoomPorcent(rango)
+  @Get('forMonths/typeRoom/porcent')
+  async findBookingsByMonthsWithTypeRoomPorcent(@Body() infoRango) {
+    const { rango } = infoRango;
+    return await this.bookingService.findBookingsByMonthsWithTypeRoomPorcent(
+      rango,
+    );
   }
 
   @Post()
   @UseInterceptors(dataBookingDatesInterceptor)
-  async makeBooking(@Body() infoBooking: MakeBookingDto){
-    return await this.bookingService.makeBooking(infoBooking)
+  async makeBooking(@Body() infoBooking: MakeBookingDto) {
+    return await this.bookingService.makeBooking(infoBooking);
   }
 
-  @Get(":id")
-  async getAllBookingsById(@Param("id") id: string){
-    return await this.bookingService.getAllBookingsById(id)
-  } 
+  @Get(':id')
+  async getAllBookingsById(@Param('id') id: string) {
+    return await this.bookingService.getAllBookingsById(id);
+  }
+
+  @Post('cancel')
+  async cancelBooking(@Body() data) {
+    const { userId, bookingId } = data;
+    return await this.bookingService.cancelBooking(userId, bookingId);
+  }
 }
